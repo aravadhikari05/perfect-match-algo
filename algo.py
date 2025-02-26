9bc3a3ef4eae3f28b398ecbbed91ea537f45fd3e
arav <aravadhikari05@gmail.com>
2025-02-26 00:50:41 -0800

    added skeleton code and data loader

---
+++ algo.py
@@ -0,0 +1,81 @@
+from itertools import combinations
+import pandas as pd
+
+#TODO add more exclusions and implement generate matches
+
+#First create score between 2 users(higher score = better match). Normalize?
+
+def calculate_score(user1, user2):
+    # -----junk gpt code - change after we get data -----
+
+    # 1. Common interests (higher = better match)
+    common_interests = len(set(user1["interested_in_majors"]) & set(user2["interested_in_majors"]))
+    
+    # 2. Age similarity (if 'prefers_same_age' is enabled)
+    age_difference = abs(user1["age"] - user2["age"])
+    age_score = 1 if (user1["prefers_same_age"] and user2["prefers_same_age"] and age_difference <= 2) else 0
+    
+    # 3. Personality Score Difference (smaller difference = better match)
+    personality_difference = abs(user1["personality_score"] - user2["personality_score"])
+    personality_score = 1 - personality_difference  # Normalize to 0-1 scale
+    
+    # Weighted score calculation
+    match_score = (2 * common_interests) + (1.5 * age_score) + (2 * personality_score)
+    
+    #-----------------------------------------------------
+    # TODO Normalize?
+    return match_score
+
+#Above function is O(n^2) - need exclusions
+#Next create matches scores for each combination of 2 users (minus exceptions)
+
+def generate_match_scores(data):
+    matches = []
+
+    user_list = data.to_dict(orient="records")
+
+    for user1, user2 in combinations(user_list, 2):
+
+        #Exclusions:
+
+        #List of potential exclusions: 
+        #gender doesn't match preferred gender, age doesnt match preferred age range, race?, religion?
+
+        #1. If gender not in preferred genders. 
+        if (user1["gender"] not in user2["preferences"].get("preferred_genders", []) or
+            user2["gender"] not in user1["preferences"].get("preferred_genders", [])):
+            continue #skips to next iteration
+
+        #2.
+
+
+
+        score = calculate_score(user1, user2)
+        
+        # Store the match if it passes filters
+        matches.append({
+            "user1": user1["name"],
+            "user2": user2["name"],
+            "match_score": score
+        })
+
+
+    scores = pd.DataFrame(matches)
+    #scores = scores.sort_values(by="match_score", ascending=False)
+    #scores.to_csv("filtered_match_results.csv", index=False)
+
+    return scores
+
+
+
+#Third, create a simple matching algorithm (this is temporary, will be improved later):
+#When creating matches, each user can get a garuntee of 2 matches, and more matches get added onto a users match list
+#Example, user 1s highest match scores are with (user 2 and user 3) but user 2s highest scores are with (user 4 and user 5).
+#then user 1 will have user 2 and user 3 as matches, and user 2 will have user 4, user 5, AND user 1 as matches.
+
+#idea is to create dictionary of matches for each user. 
+# key will be user and the value will be a list of matches for that user
+
+#things to consider is how many matches garunteed per user (3?), other potential algorithms
+def generate_matches(scores):
+    pass
---
+++ data_loader.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#Data is taken from google form (csv file)
+#Data will be organized as timestamp as first column, then rest of the columns
+#Set name or email as column index
+
+#TODO: create a better mock_data.csv to test our code
+#TODO: convert all data to lowercase (example would be when a question asks for 3 words to describe yourself and they enter upercase)
+#TODO: may need to bin some types of data. example: words to describe yourself will have too many unique values, so create an algorithm to bin them
+#      another example is age or height depending on how the data is entered
+
+
+def process_data(data_file='mock_data.csv'):
+    
+    data = pd.read_csv(data_file)
+
+    #manually rename the columns from questions to values that are easy to work with
+    #example code to test
+    column_names = {'Name': 'name', 
+                    'Age': 'age', 
+                    'Gender': 'gender', 
+                    'Do you prefer the same age?': 'prefer_same_age', 
+                    'Preferences': 'preferences'
+                }
+    data = data.rename(columns=column_names)
+    #data.timestamp = pd.to_datetime(data.timestamp)
+    data.set_index('name', inplace=True) #set this to name or email
+    print(data.head())
+
+    return data
