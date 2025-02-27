086470fc3b8560f89ad8297fe780cbf288be9818
Meghana12344 <138737345+Meghana12344@users.noreply.github.com>
2025-02-26 16:06:37 -0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,10 +4,10 @@ import pandas as pd
 #Data will be organized as timestamp as first column, then rest of the columns
 #Set name or email as column index
 
-#TODO: create a better mock_data.csv to test our code
-#TODO: convert all data to lowercase (example would be when a question asks for 3 words to describe yourself and they enter upercase)
+#TODO: create a better mock_data.csv to test our code 
+#TODO: convert all data to lowercase (example would be when a question asks for 3 words to describe yourself and they enter upercase) - DONE
 #TODO: may need to bin some types of data. example: words to describe yourself will have too many unique values, so create an algorithm to bin them
-#      another example is age or height depending on how the data is entered
+#      another example is age or height depending on how the data is entered 
 
 
 def process_data(data_file='mock_data.csv'):
@@ -22,9 +22,13 @@ def process_data(data_file='mock_data.csv'):
                     'Do you prefer the same age?': 'prefer_same_age', 
                     'Preferences': 'preferences'
                 }
+
+    
     data = data.rename(columns=column_names)
     #data.timestamp = pd.to_datetime(data.timestamp)
     data.set_index('name', inplace=True) #set this to name or email
+    #makes data lowercase
+    data = data.apply(lambda col: col.str.lower() if col.dtype == 'object' else col)
     print(data.head())
-
+    
     return data
